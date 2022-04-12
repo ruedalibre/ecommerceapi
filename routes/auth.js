e7279198c43bc101eaf72a27013063b45505819a
ruedalibre <andresruedalibre@gmail.com>
2022-04-12 18:20:54 -0500

    jwt configs

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const User = require("../models/User");
 // importo librería de Crypto JS para encriptar las contraseñas de usuario
 const CryptoJS = require("crypto-js");
+// importo JWT para mayores medidas de seguridad usando un token
+const jwt = require("jsonwebtoken");
 
 // REGISTER
 router.post("/register", async (req, res)=> {
@@ -32,11 +34,22 @@ router.post("/login", async (req, res)=>{
         const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
         // si la contraseña no coincide:
         OriginalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
-
-        const { password, ...others } = user;
+        
+        // se crea el objeto que agrega el token de JWT al objeto original del user
+        const accessToken = jwt.sign(
+            {
+                id:user._id, 
+                isAdmin: user.isAdmin,
+            }, 
+            process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+        
+        // El formato _doc es propio de MongoDB y siempre guarda los documentos en dicho folder 
+        const { password, ...others } = user._doc;
 
         // Si los datos de usuario y contraseña están correctos: 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     }catch(err){
         res.status(500).json(err);
     }
---
+++ routes/verifyToken.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken")
+
+
+const verifyToken = (req, res, next)=> {
+    const authHeader = req.headers.token
+    if(authHeader){
+
+    }else{
+        return res.status(401).json("You are not authenticated")
+    }
+};
