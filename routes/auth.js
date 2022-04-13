c779f4dd77ebf91e8795a7203f11ddb476710ad0
ruedalibre <andresruedalibre@gmail.com>
2022-04-12 21:55:56 -0500

    crud methods part I

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,9 @@ router.post("/register", async (req, res)=> {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+        password: CryptoJS.AES.encrypt(
+            req.body.password, 
+            process.env.PASS_SEC).toString(),
     }); 
 
     try{
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,10 +2,39 @@ const jwt = require("jsonwebtoken")
 
 
 const verifyToken = (req, res, next)=> {
-    const authHeader = req.headers.token
-    if(authHeader){
+    const authHeader = req.headers.token;
+    if(authHeader) {
+        // Accede al segundo elemento del token porque primero se dividió con split y el el token como tal está en la segunda posición del array
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.JWT_SEC, (err, user)=> {
+            if(err) res.status(403).json("Token is not valid");
+            req.user = user;
+            next();
+        });
 
     }else{
         return res.status(401).json("You are not authenticated")
     }
 };
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next();
+        } else {
+            res.status(403).json("You are not allowed to do that!")
+        }
+    });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.isAdmin){
+            next();
+        } else {
+            res.status(403).json("You are not allowed to do that!")
+        }
+    });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin }
