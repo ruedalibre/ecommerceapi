d7aebffc8a612f2e99f572f1cb206e0634e9e816
ruedalibre <andresruedalibre@gmail.com>
2022-04-14 11:05:39 -0500

    CRUD order and cart

---
+++ routes/order.js
@@ -0,0 +1,72 @@
+const Order = require("../models/Order");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
+const router = require("express").Router();
+
+// CREATE
+router.post("/", verifyToken, async (req,res) => {
+    const newOrder = new Order(req.body)
+
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    } catch(err) {
+        res.status(500).json(err)
+    } 
+});
+
+// UPDATE FUNCTION
+router.put("/:id", verifyTokenAndAdmin, async (req, res)=> {
+    try{
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id, 
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedOrder);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// // // DELETE FUNCTION
+router.delete("/:id", verifyTokenAndAdmin, async(req, res) => {
+    try{
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Order has been deleted...");
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET USER ORDER ---> en este caso no se usa token porque cualquier persona debe poder ver los productos
+router.get("/find/:userId", verifyTokenAndAuthorization, async(req, res) => {
+    try{
+        const orders = await Order.find({userId: req.params.id})
+        res.status(200).json(orders);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET ALL
+
+router.get("/", verifyTokenAndAdmin, async (req,res) => {
+    try {
+        const orders = await Order.find();
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET MONTHLY INCOME
+router.get("/income", verifyTokenAndAdmin, async(req,res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() -1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() -1));
+});
+
+module.exports = router;
